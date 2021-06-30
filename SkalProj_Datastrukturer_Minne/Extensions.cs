ce8a889085b82e4353bcef1d42922bf6b717ae89
buffelsafari <buffelsafari@hotmail.com>
2021-06-30 10:23:25 +0200

    <><>

---
+++ SkalProj_Datastrukturer_Minne/Extensions.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    
+
+    static class Extensions
+    {        
+        private static readonly Dictionary<Type, string[]> dictionary;
+        
+        private delegate int Capacity();
+        private delegate void Add(string str);
+        private delegate string Remove();
+        private delegate bool RemoveItem(string str);
+
+        static Extensions()
+        {           
+            
+            dictionary = new Dictionary<Type, string[]>();
+
+            dictionary.Add(typeof(Remove), new string[]{ "Dequeue", "Pop" });
+            dictionary.Add(typeof(RemoveItem), new string[] { "Remove" });
+            dictionary.Add(typeof(Add), new string[] { "Add", "Enqueue", "Push" });
+            dictionary.Add(typeof(Capacity), new string[] { "get_Capacity" });
+        }
+
+        public static void Examine(this ICollection collection)
+        {
+            // försökte mig på att abstraktisera övning 1,2,3
+            // men det blev knasigare än väntat då de olika collectionerna inte var så lika som jag först trodde
+            // använder reflection för att tilldela delegaterna collectionernas funktionalitet
+            
+
+            Capacity capacity= collection.AssignDelegate<Capacity>(dictionary);
+            Add add= collection.AssignDelegate<Add>(dictionary);
+            RemoveItem removeItem= collection.AssignDelegate<RemoveItem>(dictionary);
+            Remove remove=collection.AssignDelegate<Remove>(dictionary);
+
+            ShowCommands();
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+
+                int? cap = capacity?.Invoke();
+                Console.Write($"the collection contains {collection.Count} elements");
+                if (cap != null)
+                {
+                    Console.Write($" and the capacity is {cap} elements");
+                }
+                Console.Write(".\n");
+
+
+                string line = Console.ReadLine();
+                char nav = '0';
+                if (line.Length > 0)
+                {
+                    nav = line[0];
+                    line = line[1..];
+                }
+
+                switch (nav)
+                {
+                    case '+':
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            add?.Invoke(line);
+                        }
+                        break;
+                    case '-':
+                        if (collection.Count>0)
+                        {
+                            remove?.Invoke();
+                            removeItem?.Invoke(line);
+                        }
+                        break;
+                    case 'q':   // use 'q' to exit to main menu
+                        isRunning = false;
+                        break;
+                    case 's':
+                        foreach (string s in collection)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        break;
+                    default:
+                        ShowCommands();
+                        break;
+
+                }
+            }            
+        }
+
+        private static void ShowCommands()
+        {
+            Console.WriteLine("use +, -, s or q");
+        }
+
+        // skapar ett delegat från en ICollection av typen T, använder en dictonary för att hitta lämplig metod
+        private static T AssignDelegate<T>(this ICollection collection, Dictionary<Type, string[]> dict) where T:Delegate
+        {
+            string[] arr;
+            dict.TryGetValue(typeof(T), out arr);
+
+
+            foreach (string name in arr)
+            {
+                MethodInfo methodInfo = collection.GetType().GetMethod(name);
+                if (methodInfo != null)
+                {                    
+                    return (T)methodInfo.CreateDelegate(typeof(T), collection);
+                }
+            }
+            return null;
+        }
+
+
+
+
+       
+
+    }
+}
--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -10,6 +12,15 @@ namespace SkalProj_Datastrukturer_Minne
         /// <param name="args"></param>
         static void Main()
         {
+            //svar:
+            // 1. Stacken är minnet som används av programmtråden för att hålla lokala data i en funktion och tas bort direkt när funktionen körts
+            // stacken är ordnad som en först in först ut struktur.
+            // Heapen är till för att lagra dynamisk data, te.x när du allokerar en objectinstans med 'new', detta minne hanteras i c# av en garbage collector
+            // som ansvarar för att orefererat minne friges och ev deframentation.
+            // 2. Value type är en typ som är ett värde direkt, referens type är en referens som pekar på en minnesplats där värdet finns.
+            // 3. Den första retunerar 3 för att x tilldelas värdet 3 och retuners senare, den andra så tilldelas y samma referens som x, därmed ändras både x,y när y ändras ->4 retuneras
+
+
 
             while (true)
             {
@@ -18,6 +29,9 @@ namespace SkalProj_Datastrukturer_Minne
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
                     + "\n4. CheckParanthesis"
+                    + "\n5. The Recursion thing"
+                    + "\n6. The Iteration thing"
+                    + "\n7. Reverse"
                     + "\n0. Exit the application");
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
@@ -43,6 +57,15 @@ namespace SkalProj_Datastrukturer_Minne
                     case '4':
                         CheckParanthesis();
                         break;
+                    case '5':
+                        Recursion();
+                        break;
+                    case '6':
+                        Iteration();
+                        break;
+                    case '7':
+                        Reverse();
+                        break;
                     /*
                      * Extend the menu to include the recursive 
                      * and iterative exercises.
@@ -55,6 +78,106 @@ namespace SkalProj_Datastrukturer_Minne
                         break;
                 }
             }
+        }
+
+        private static void Iteration()
+        {
+            
+
+
+            Console.WriteLine("Iterative even?");
+            string line = Console.ReadLine();
+
+            int n = 0;
+            int.TryParse(line, out n);
+
+            Console.WriteLine($"The {n}'th even number is {IterativeEven(n)}");
+
+
+            Console.WriteLine("Fibonnaci Iteration?");
+            line = Console.ReadLine();
+
+            n = 0;
+            int.TryParse(line, out n);
+
+
+            Console.WriteLine($"The {n}'th fibonnacci number is {IterativeFibon(n)}");
+
+
+
+        }
+
+        private static int IterativeEven(int n)
+        {
+            int output = 0;
+            for (int i = 0; i < n; i++)
+            {
+                output+=2;
+            }
+
+            return output;
+        }
+
+        private static int IterativeFibon(int n)
+        {
+            // svar. en iterativ lösning använder betydligt minder stackminne då den endast körs ifrån en metod
+            // en rekursiv behöver lagra ett helt träd med metoddata på stacken
+
+            int[] array = new int[3];
+            array[1] = 1;
+            array[2] = 1;
+
+            for (int i = 0; i < n-2; i++)
+            {
+                array[0] = array[1];
+                array[1] = array[2];
+                array[2] = array[0] + array[1];
+
+            }
+            return array[2];
+        }
+
+        private static void Recursion()
+        {
+            
+
+            Console.WriteLine("Recursive even?");
+            string line = Console.ReadLine();
+
+            int n = 0;
+            int.TryParse(line, out n);
+
+            Console.WriteLine($"The {n}'th even number is {RecursiveEven(n)}");
+
+
+            Console.WriteLine("Fibonnaci Recursive?");
+            line = Console.ReadLine();
+
+            n = 0;
+            int.TryParse(line, out n);
+
+
+            Console.WriteLine($"The {n}'th fibonnacci number is {Fibon(n)}");
+
+        }
+
+        private static int RecursiveEven(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            return (RecursiveEven(n-1)+2);
+        }
+
+        private static int Fibon(int n)
+        {
+            
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return Fibon(n-1) + Fibon(n-2);
         }
 
         /// <summary>
@@ -78,6 +201,22 @@ namespace SkalProj_Datastrukturer_Minne
             //string value = input.substring(1);
 
             //switch(nav){...}
+
+            //ExamineCollection<List<string>>();
+
+
+
+            List<string> list = new List<string>();
+            list.Examine();   // en extension metod i separat fil
+
+            // 2. listans capacitet ökar när det finns mer element än kapacitet
+            // 3. en dubblering
+            // 4. för att det tar tid att allokera en ny array och kopiera över den den gamla till den ny samt radera den gamla
+            // 5. Nej, inte automatiskt.
+            // 6. När man har en förutsägbar storlek.
+
+            
+
         }
 
         /// <summary>
@@ -90,6 +229,9 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            Queue<string> queue = new Queue<string>();
+            queue.Examine();
         }
 
         /// <summary>
@@ -102,10 +244,57 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+
+            Stack<string> stack = new Stack<string>();
+            stack.Examine();
+
         }
+
+        static void Reverse()
+        {
+            Console.WriteLine("Type something to be reversed:");
+            string line = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            char[] charArray = line.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                stack.Push(charArray[i]);
+            }
+
+            while(stack.Count!=0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.Write("\n");
+        }
+
 
         static void CheckParanthesis()
         {
+            // Här har jag använt mig av en rekursion, jag missade instruktionerna litegrann
+            // Då det är olika nivåer av instruktioner i kommentarerna vs övningsbeskrivningen
+            // Men eftersom en rekursion kan betraktas som en stack typ av datastruktur är det ok iaf.
+            // funktionen delar dessutom upp resten av strängen som förberedelse för vidare tolkning.
+
+            Console.WriteLine("Type a line to check if paranthesis is correct");
+
+            string line = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("------------------------------------------------------------------");
+                Para(line.AsSpan(), 0, ' ', 0);
+                Console.WriteLine("------------------------------------------------------------------");
+                Console.WriteLine("everything is fine!");
+            }
+            catch(Exception e)
+            {                
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.WriteLine("------------------------------------------------------------------");
             /*
              * Use this method to check if the paranthesis in a string is Correct or incorrect.
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
@@ -113,6 +302,85 @@ namespace SkalProj_Datastrukturer_Minne
              */
 
         }
+
+        static int Para(ReadOnlySpan<char> span, int index, char expectedParantes, int level)
+        {
+            // använder en span? en indexposition för att göra det lite minnesvänligare
+            // har också satt in en levelparameter som inte behövs men det kan vara intressant
+            // vid vidare utvecking av syntaxträdet
+
+            Console.WriteLine("at level "+level+ "  parantesis "+expectedParantes);
+            int start = index;
+            while(index<span.Length)
+            {                
+                switch (span[index])
+                {
+                    case ']':                             
+                    case '}':                                         
+                    case '>':                                          
+                    case ')':
+                        if (expectedParantes != span[index])
+                        {
+                            throw new ArgumentException("Parantes missmatch at position "+index);
+                        }
+                        // text innom paranteser
+                        Console.WriteLine("internal:" + span.Slice(start, index - start).ToString());
+                        return index;
+                    
+
+
+                    case '[':
+                    case '{':
+                    case '<':
+                    case '(':
+                        // text före en parantes
+                        Console.WriteLine("external:" +span.Slice(start, index - start).ToString());
+                        index = Para(span, index + 1, GetMatchingParantes(span[index]), level+1);
+                        start = index+1;
+                        break;
+                    
+                        
+                
+                }                
+                index++;
+            }
+
+            if (expectedParantes != ' ')
+            {
+                // när det finns "öppna paranteser när strängen är slut"
+                throw new ArgumentException("Missing parantes at position " + index);                
+            }
+            if (start < span.Length)
+            {     
+                // ev text i slutet av strängen
+                Console.WriteLine("at level " + level + "  parantesis " + expectedParantes);
+                Console.WriteLine("last:" + span.Slice(start).ToString());
+            }
+
+            return index;
+        }
+
+        // hjälpmetod till Para
+        private static char GetMatchingParantes(char input)
+        {
+            switch (input)
+            {
+                case '[':
+                    return ']';                    
+                case '{':
+                    return '}';                    
+                case '<':
+                    return '>';                    
+                case '(':
+                    return ')'; 
+            }
+            return ' ';
+        }
+
+
+        
+
+        
 
     }
 }
